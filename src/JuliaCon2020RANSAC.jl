83b242500ae71d9070eb8e6d46052f69c7943c6e
Tamás Cserteg <csertegt@gmail.com>
2020-06-04 12:12:45 +0200

    add Remark presentation skeleton

--- src/JuliaCon2020RANSAC.jl
+++ src/JuliaCon2020RANSAC.jl
@@ -1,3 +1,17 @@
 module JuliaCon2020RANSAC
 
+import Remark
+
+
+export  makeslides,
+        openslides
+
+
+const SLIDE_FOLDER = joinpath(dirname(@__DIR__), "slides")
+const SLIDE_FILE = joinpath(SLIDE_FOLDER, "JuliaCon2020.md")
+
+makeslides() = Remark.slideshow(SLIDE_FILE, SLIDE_FOLDER, options=Dict("ratio"=>"16:9"))
+openslides() = Remark.open(SLIDE_FOLDER)
+
+
 end # module
